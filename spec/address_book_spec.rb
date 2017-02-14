f4292072584a1819d2d6f3caa0fc605d3bb41ee7
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-14 13:01:06 -0500

    add_entry tests failed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+    let(:book) {AddressBook.new}
     describe "attributes" do
-        let(:book) {AddressBook.new}
         it "responds to entries" do
             expect(book).to respond_to(:entries)
         end
@@ -15,4 +15,26 @@ RSpec.describe AddressBook do
             expect(book.entries.size).to eq(0)
         end
     end
+
+    describe "#add_entry" do
+        # let(:book) {AddressBook.new}
+        it "adds only one entry to the address book" do
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+            expect(book.entries.size).to eq(1)
+        end
+
+        it "adds the correct information to entries" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            new_entry = book.entries[0]
+
+            expect(new_entry.name).to eq('Ada Lovelace')
+            expect(new_entry.phone_number).to eq('010.012.1815')
+            expect(new_entry.email).to eq('augusta.king@lovelace.com')
+
+        end
+    end
+
+
 end
