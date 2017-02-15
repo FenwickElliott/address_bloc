e3badf2e664e0053be3fb5bba5edf7dfa9b8760f
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-15 12:07:12 -0500

    cp9 complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -76,6 +76,15 @@ class MenuController
     end
 
     def search_entries
+        print "Search by name: "
+        name = gets.chomp
+        match = address_book.binary_search(name)
+        if match
+            puts match.to_s
+            search_submenu(match)
+        else
+            puts "No match found for #{name}"
+        end
     end
 
     def read_csv
@@ -152,5 +161,30 @@ class MenuController
         system "clear"
         puts "Updated entry:"
         puts entry
+    end
+
+    def search_submenu(entry)
+        puts "\nd - delete entry"
+        puts "e - edit this entry"
+        puts "m - return to main menu"
+        selection = gets.chomp
+        case selection
+        when "d"
+            system "clear"
+            delete_entry(entry)
+            main_menu
+        when "e"
+            edit_entry(entry)
+            system "clear"
+            main_menu
+        when "m"
+            system "clear"
+            main_menu
+        else
+            system "clear"
+            puts "#{selection} is not valid input"
+            puts entry.to_s
+            search_submenu(entry)
+        end
     end
 end
